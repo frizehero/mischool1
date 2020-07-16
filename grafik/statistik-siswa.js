7ac56dadddcf4d603882dcdb2b43626f3c0a5f63
PutraEkaPurwanto <62229634+PutraEkaPurwanto@users.noreply.github.com>
2020-07-16 10:17:26 +0700

    dijaok candra

--- grafik/statistik-siswa.js
+++ grafik/statistik-siswa.js
@@ -821,110 +821,24 @@ var KTApexChartsDemo = function () {
 	var _demo10 = function () {
 		const apexChart = "#chart_10";
 		var options = {
-			series: [{
-				name: 'Jan',
-				data: generateData(20, {
-					min: -30,
-					max: 55
-				})
-			},
-			{
-				name: 'Feb',
-				data: generateData(20, {
-					min: -30,
-					max: 55
-				})
-			},
-			{
-				name: 'Mar',
-				data: generateData(20, {
-					min: -30,
-					max: 55
-				})
-			},
-			{
-				name: 'Apr',
-				data: generateData(20, {
-					min: -30,
-					max: 55
-				})
-			},
-			{
-				name: 'May',
-				data: generateData(20, {
-					min: -30,
-					max: 55
-				})
-			},
-			{
-				name: 'Jun',
-				data: generateData(20, {
-					min: -30,
-					max: 55
-				})
-			},
-			{
-				name: 'Jul',
-				data: generateData(20, {
-					min: -30,
-					max: 55
-				})
-			},
-			{
-				name: 'Aug',
-				data: generateData(20, {
-					min: -30,
-					max: 55
-				})
-			},
-			{
-				name: 'Sep',
-				data: generateData(20, {
-					min: -30,
-					max: 55
-				})
-			}
-			],
+			series: [40,20],
 			chart: {
-				height: 350,
-				type: 'heatmap',
+				width: 380,
+				type: 'donut',
 			},
-			plotOptions: {
-				heatmap: {
-					shadeIntensity: 0.5,
-
-					colorScale: {
-						ranges: [{
-							from: -30,
-							to: 5,
-							name: 'low',
-							color: success
-						},
-						{
-							from: 6,
-							to: 20,
-							name: 'medium',
-							color: primary
-						},
-						{
-							from: 21,
-							to: 45,
-							name: 'high',
-							color: warning
-						},
-						{
-							from: 46,
-							to: 55,
-							name: 'extreme',
-							color: danger
-						}
-						]
+			labels: ['SMPN 1 LAWANG','SMPN 1 SINGOSARI'],
+			responsive: [{
+				breakpoint: 480,
+				options: {
+					chart: {
+						width: 200
+					},
+					legend: {
+						position: 'bottom'
 					}
 				}
-			},
-			dataLabels: {
-				enabled: false
-			},
+			}],
+			colors: [primary, success, warning, danger, info]
 		};
 
 		var chart = new ApexCharts(document.querySelector(apexChart), options);
