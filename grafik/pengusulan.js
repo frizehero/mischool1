3ce246d6969ef93a7a1b3aced08b8eb1e957fb3d
dwicahyonoe <62232464+dwicahyonoe@users.noreply.github.com>
2020-07-20 10:20:19 +0700

    pengusulan

---
+++ grafik/pengusulan.js
@@ -0,0 +1,124 @@
+"use strict";
+var KTDatatablesDataSourceHtml = function() {
+
+	var initTable1 = function() {
+		var table = $('#kt_datatable1');
+
+		// begin first table
+		table.DataTable({
+			responsive: true,
+			columnDefs: [
+				{
+					targets: -1,
+					title: 'Actions',
+					orderable: false,
+					render: function(data, type, full, meta) {
+						return '\
+							<div class="dropdown dropdown-inline">\
+							  	<div class="dropdown-menu dropdown-menu-sm dropdown-menu-right">\
+									<ul class="nav nav-hoverable flex-column">\
+							    		<li class="nav-item"><a class="nav-link" href="#"><i class="nav-icon la la-edit"></i><span class="nav-text">Edit Details</span></a></li>\
+							    		<li class="nav-item"><a class="nav-link" href="#"><i class="nav-icon la la-leaf"></i><span class="nav-text">Update Status</span></a></li>\
+							    		<li class="nav-item"><a class="nav-link" href="#"><i class="nav-icon la la-print"></i><span class="nav-text">Print</span></a></li>\
+									</ul>\
+							  	</div>\
+							</div>\
+							<a href="javascript:;" class="btn btn-sm btn-clean btn-icon" title="Edit details">\
+								<i class="la la-edit"></i>\
+							</a>\
+							<a href="javascript:;" class="btn btn-sm btn-clean btn-icon" title="Delete">\
+								<i class="la la-trash"></i>\
+							</a>\
+						';
+					},
+				},
+
+
+			],
+		});
+		var table = $('#kt_datatable2');
+			table.DataTable({
+			responsive: true,
+			columnDefs: [
+				{
+					targets: -1,
+					title: 'Actions',
+					orderable: false,
+					render: function(data, type, full, meta) {
+						return '\
+							<div class="dropdown dropdown-inline">\
+								<a href="javascript:;" class="btn btn-sm btn-clean btn-icon" data-toggle="dropdown">\
+	                                <i class="la la-cog"></i>\
+	                            </a>\
+							  	<div class="dropdown-menu dropdown-menu-sm dropdown-menu-right">\
+									<ul class="nav nav-hoverable flex-column">\
+							    		<li class="nav-item"><a class="nav-link" href="#"><i class="nav-icon la la-edit"></i><span class="nav-text">Edit Details</span></a></li>\
+							    		<li class="nav-item"><a class="nav-link" href="#"><i class="nav-icon la la-leaf"></i><span class="nav-text">Update Status</span></a></li>\
+							    		<li class="nav-item"><a class="nav-link" href="#"><i class="nav-icon la la-print"></i><span class="nav-text">Print</span></a></li>\
+									</ul>\
+							  	</div>\
+							</div>\
+							<a href="javascript:;" class="btn btn-sm btn-clean btn-icon" title="Edit details">\
+								<i class="la la-edit"></i>\
+							</a>\
+							<a href="javascript:;" class="btn btn-sm btn-clean btn-icon" title="Delete">\
+								<i class="la la-trash"></i>\
+							</a>\
+						';
+					},
+				},
+
+
+			],
+		});
+			var table = $('#kt_datatable3');
+			table.DataTable({
+			responsive: true,
+			columnDefs: [
+				{
+					targets: -1,
+					title: 'Actions',
+					orderable: false,
+					render: function(data, type, full, meta) {
+						return '\
+							<div class="dropdown dropdown-inline">\
+								<a href="javascript:;" class="btn btn-sm btn-clean btn-icon" data-toggle="dropdown">\
+	                                <i class="la la-cog"></i>\
+	                            </a>\
+							  	<div class="dropdown-menu dropdown-menu-sm dropdown-menu-right">\
+									<ul class="nav nav-hoverable flex-column">\
+							    		<li class="nav-item"><a class="nav-link" href="#"><i class="nav-icon la la-edit"></i><span class="nav-text">Edit Details</span></a></li>\
+							    		<li class="nav-item"><a class="nav-link" href="#"><i class="nav-icon la la-leaf"></i><span class="nav-text">Update Status</span></a></li>\
+							    		<li class="nav-item"><a class="nav-link" href="#"><i class="nav-icon la la-print"></i><span class="nav-text">Print</span></a></li>\
+									</ul>\
+							  	</div>\
+							</div>\
+							<a href="javascript:;" class="btn btn-sm btn-clean btn-icon" title="Edit details">\
+								<i class="la la-edit"></i>\
+							</a>\
+							<a href="javascript:;" class="btn btn-sm btn-clean btn-icon" title="Delete">\
+								<i class="la la-trash"></i>\
+							</a>\
+						';
+					},
+				},
+
+
+			],
+		});
+	};
+
+	return {
+
+		//main function to initiate the module
+		init: function() {
+			initTable1();
+		},
+
+	};
+
+}();
+
+jQuery(document).ready(function() {
+	KTDatatablesDataSourceHtml.init();
+});
